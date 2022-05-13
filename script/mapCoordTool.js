f0e2370f33ed46069679630a5703bb3ba56bf0db
kayki.garcia <kayki.garcia@lighthouseit.com.br>
2022-05-13 11:14:03 -0300

    add: new mapping tools

--- script/mapCoordTool.js
+++ script/mapCoordTool.js
@@ -1,33 +1,67 @@
-let sides = 0;
-let coords = [];
+class Mapper {
+  constructor() {
+    this._coords = [];
+    this._isMapping = false;
+    this._features = [];
+  }
 
-function getArray(lng, lat) {
-  coords.push([lng, lat]);
-  if (coords.length >= sides) {
-    const name = prompt("nome: ");
-    const feature = {
-      type: "Feature",
-      properties: {
-        name,
-        openTime: {
-          open: 510,
-          close: 1230,
-        },
+  static featureTemplate = (name, coords) => ({
+    type: "Feature",
+    properties: {
+      name,
+      openTime: {
+        open: 510,
+        close: 1230,
       },
-      geometry: {
-        type: "Polygon",
-        coordinates: [coords],
-      },
-    };
-    coords = [];
-    sides = 0;
-    console.log(JSON.stringify(feature));
+    },
+    geometry: {
+      type: "Polygon",
+      coordinates: [coords],
+    },
+  });
+
+  _startMapping() {
+    this._coords.length = 0;
+  }
+
+  _stopMapping() {
+    const name = prompt("Insira o nome da loja");
+    const newFeature = Mapper.featureTemplate(name, this._coords);
+    this._features.push(newFeature);
+  }
+
+  addCoord(newCoord) {
+    this._coords.push(newCoord);
+  }
+
+  get lastFeature() {
+    return this._features[this._features.length - 1];
+  }
+
+  get features() {
+    return this._features;
+  }
+
+  switch() {
+    this._isMapping ? this._stopMapping() : this._startMapping();
+    this._isMapping = !this._isMapping;
   }
 }
 
+const mapper = new Mapper();
+
 map.addEventListener("contextmenu", ({ latlng: { lat, lng } }) => {
-  if (!sides) {
-    sides = prompt("vertices: ");
+  mapper.addCoord([lng, lat]);
+});
+
+document.addEventListener("keydown", ({ key }) => {
+  console.log(key);
+  const action = {
+    s: () => mapper.switch(),
+    c: () => navigator.clipboard.writeText(JSON.stringify(mapper.lastFeature)),
+    a: () => navigator.clipboard.writeText(JSON.stringify(mapper.features)),
+  };
+  if (key in action) {
+    action[key]();
   }
-  getArray(lng, lat); // Invertido, pois é assim no geoJSON
 });
